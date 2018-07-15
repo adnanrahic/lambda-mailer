4a22f3380c65d8d7d76d85af64b6c366c600b2fa
adnanrahic <ado.raha198@gmail.com>
2018-07-15 14:54:47 +0200

    added error handling

--- lambdaMailer.js
+++ lambdaMailer.js
@@ -16,8 +16,26 @@ module.exports = (options) => {
     }
   }
 
+  function generateError (code, err) {
+    console.log(err)
+    return {
+      statusCode: code,
+      headers: {
+        'Access-Control-Allow-Origin': myDomain,
+        'Access-Control-Allow-Headers': 'x-requested-with',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify(err.message)
+    }
+  }
+
   function generateEmailParams (body) {
     const { email, name, content } = JSON.parse(body)
+    console.log(email, name, content)
+    if (!(email && name && content)) {
+      throw new Error('Missing parameters! Make sure to add parameters \'email\', \'name\', \'content\'.')
+    }
+
     return {
       Source: myEmail,
       Destination: { ToAddresses: [myEmail] },
@@ -43,7 +61,7 @@ module.exports = (options) => {
       const data = await ses.sendEmail(emailParams).promise()
       return generateResponse(200, data)
     } catch (err) {
-      return generateResponse(500, err)
+      return generateError(500, err)
     }
   }
 
